fdc17ced3ed9d517630f497ecfdad5184c9df992
toyib <toyibseha@gmail.com>
2020-05-18 17:23:36 +0700

    Android Fast Networking updates

--- app/src/main/java/com/toyibnurseha/dicodingsubmission/data/SearchViewModel.kt
+++ app/src/main/java/com/toyibnurseha/dicodingsubmission/data/SearchViewModel.kt
@@ -4,14 +4,15 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.AsyncHttpResponseHandler
-import cz.msebera.android.httpclient.Header
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
 import org.json.JSONObject
 
 class SearchViewModel : ViewModel() {
 
-    val listUser = MutableLiveData<ArrayList<UserInfo>>()
+    private val listUser = MutableLiveData<ArrayList<UserInfo>>()
 
     companion object {
         const val FAIL = "failed"
@@ -22,47 +23,38 @@ class SearchViewModel : ViewModel() {
         val userDetail = ArrayList<UserInfo>()
         val apiKey = "token 3fb52402ff897f4ceb4781adbd5222bbd4481925"
         val url = "https://api.github.com/search/users?q=$username"
-        val client = AsyncHttpClient()
-        client.addHeader("Authorization", apiKey)
-        client.addHeader("User-Agent", "request")
-        client.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?
-            ) {
-                try {
 
-                    val result = String(responseBody!!)
-                    val responseObject = JSONObject(result)
-                    val items = responseObject.getJSONArray("items")
+        AndroidNetworking.get(url)
+            .setPriority(Priority.MEDIUM)
+            .addHeaders("Authorization", apiKey)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject?) {
+                    try {
+                        val items = response?.getJSONArray("items")
 
-                    for (i in 0 until items.length()) {
-                        val user = items.getJSONObject(i)
-                        val userItems = UserInfo()
+                        if (items != null) {
+                            for (i in 0 until items.length()) {
+                                val user = items.getJSONObject(i)
+                                val userItems = UserInfo()
 
-                        userItems.username = user.getString("login")
-                        userItems.avatar = user.getString("avatar_url")
+                                userItems.username = user.optString("login")
+                                userItems.avatar = user.optString("avatar_url")
 
-                        userDetail.add(userItems)
+                                userDetail.add(userItems)
+                            }
+                            listUser.postValue(userDetail)
+                        }
+                    } catch (e: Exception) {
+                        Log.d(FAIL, e.message.toString())
                     }
-
-                    listUser.postValue(userDetail)
-                } catch (e: Exception) {
-                    Log.d(FAIL, e.message.toString())
                 }
-            }
 
-            override fun onFailure(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?,
-                error: Throwable?
-            ) {
-                Log.d(FAIL, error?.message.toString())
-            }
+                override fun onError(anError: ANError?) {
+                    Log.d(FAIL, anError?.message.toString())
+                }
 
-        })
+            })
     }
 
     fun getUserList(): LiveData<ArrayList<UserInfo>> {
--- app/src/main/java/com/toyibnurseha/dicodingsubmission/data/UserViewModel.kt
+++ app/src/main/java/com/toyibnurseha/dicodingsubmission/data/UserViewModel.kt
@@ -4,55 +4,61 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONArrayRequestListener
+import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
 import org.json.JSONArray
 import org.json.JSONObject
+import java.lang.Exception
 
 class UserViewModel : ViewModel() {
 
     private val apiKey = "token 3fb52402ff897f4ceb4781adbd5222bbd4481925"
     val userDetail = MutableLiveData<UserInfo>()
     val userFollowers = MutableLiveData<ArrayList<UserInfo>>()
-    val toastMessage = MutableLiveData<String>()
+    private val toastMessage = MutableLiveData<String>()
 
     companion object {
         const val FAIL = "failed"
-        const val TOAST = "error message : "
+        const val TOAST = "error message: "
     }
 
     fun setUserDetail(username: String) {
         val url = "https://api.github.com/users/$username"
-        val client = AsyncHttpClient()
-        client.addHeader("Authorization", apiKey)
-        client.addHeader("User-Agent", "request")
-        client.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?
-            ) {
-                val result = String(responseBody!!)
-                val responseObject = JSONObject(result)
-                val userInfo = UserInfo()
-                userInfo.username = responseObject.getString("login")
-                userInfo.bio = responseObject.getString("bio")
-                userInfo.avatar = responseObject.getString("avatar_url")
-                userInfo.name = responseObject.getString("name")
-                userDetail.postValue(userInfo)
-            }
 
-            override fun onFailure(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?,
-                error: Throwable?
-            ) {
-                Log.d(FAIL, error?.message.toString())
-                setToastMessage(error?.message.toString())
-            }
-        })
+        AndroidNetworking.get(url)
+            .setPriority(Priority.MEDIUM)
+            .addHeaders("Authorization", apiKey)
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener{
+                override fun onResponse(response: JSONObject?) {
+                    try {
+                        val userInfo = UserInfo()
+
+                        if (response != null) {
+                            userInfo.name = response.optString("name")
+                            userInfo.username = response.optString("login")
+                            userInfo.bio = response.optString("bio")
+                            userInfo.avatar = response.optString("avatar_url")
+                            userDetail.postValue(userInfo)
+                        }
+                    }catch (e: Exception){
+                        Log.d(FAIL, e.message.toString())
+                        setToastMessage(e.message.toString())
+                    }
+                }
+
+                override fun onError(anError: ANError?) {
+                    Log.d(FAIL, anError?.message.toString())
+                    setToastMessage(anError?.message.toString())
+                }
+
+            })
     }
 
     fun getUserDetail(): LiveData<UserInfo> {
@@ -63,44 +69,41 @@ class UserViewModel : ViewModel() {
 
         val followers = ArrayList<UserInfo>()
         val url = "https://api.github.com/users/$username/$followersParam"
-        val client = AsyncHttpClient()
-        client.addHeader("Authorization", apiKey)
-        client.addHeader("User-Agent", "request")
-        client.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?
-            ) {
-                try {
 
-                    val result = String(responseBody!!)
-                    val responseArray = JSONArray(result)
 
-                    for (i in 0 until responseArray.length()) {
-                        val follower = responseArray.getJSONObject(i)
-                        val followerItems = UserInfo()
+            AndroidNetworking.get(url)
+                .setPriority(Priority.MEDIUM)
+                .addHeaders("Authorization", apiKey)
+                .build()
+                .getAsJSONArray(object : JSONArrayRequestListener{
+                    override fun onResponse(response: JSONArray?) {
 
-                        followerItems.username = follower.getString("login")
-                        followerItems.avatar = follower.getString("avatar_url")
-                        followers.add(followerItems)
+                        try {
+                            if (response != null) {
+                                for (i in 0 until response.length()) {
+                                    val follower = response.getJSONObject(i)
+                                    val followerItems = UserInfo()
+
+                                    followerItems.username = follower.optString("login")
+                                    followerItems.avatar = follower.optString("avatar_url")
+
+                                    followers.add(followerItems)
+                                }
+                                userFollowers.postValue(followers)
+                            }
+                        }catch (e: Exception){
+                            Log.d(FAIL, e.message.toString())
+                        }
                     }
-                    userFollowers.postValue(followers)
-                } catch (e: Exception) {
-                    Log.d(FAIL, e.message.toString())
-                }
-            }
 
-            override fun onFailure(
-                statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?,
-                error: Throwable?
-            ) {
-                Log.d(FAIL, error?.message.toString())
 
-            }
-        })
+                    override fun onError(anError: ANError?) {
+                        Log.d(FAIL, anError?.message.toString())
+                        setToastMessage(anError?.message.toString())
+                    }
+
+                })
+
     }
 
     fun getFollowers(): LiveData<ArrayList<UserInfo>> {
